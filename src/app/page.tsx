cab4e2d5619dbe087f168ccd5be6bc18cf563385
Samyakt <isamyakt17@gmail.com>
2025-04-19 11:50:34 +0530

    Customized globals.css and shadcnn ui components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 export default function Home() {
   return (
-    <div>
-      <p className="text-rose-500">Hello World</p>
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant="elevated">Its button</Button>
+        </div>
+        <div>
+          <Input placeholder="Its input" />
+        </div>
+        <div>
+          <Progress value={50} />
+        </div>
+        <div>
+          <Textarea placeholder="Its textarea" />
+        </div>
+        <div>
+            <Checkbox />
+        </div>
+      </div>
     </div>
   );
 }
