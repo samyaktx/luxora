3423aec2bfe7e9dc0b433b36f1dc8012c2d5fc1a
Samyakt <isamyakt17@gmail.com>
2025-04-28 23:44:09 +0530

    feat: created sign-in/sign-up authentication

---
+++ src/app/(app)/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import { SignInView } from '@/modules/auth/ui/view/sign-in-view';
+
+const SignIn = () => {
+  return (
+    <SignInView />
+  )
+}
+
+export default SignIn;
---
+++ src/app/(app)/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import { SignUpView } from '@/modules/auth/ui/view/sign-up-view';
+
+const SignUp = () => {
+  return (
+    <SignUpView />
+  )
+}
+
+export default SignUp;
